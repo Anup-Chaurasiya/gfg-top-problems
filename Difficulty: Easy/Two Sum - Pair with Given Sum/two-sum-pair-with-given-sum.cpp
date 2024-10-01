5f4fedc17ddd2724de029dfa9cb5971056a81f3b
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-10-01 16:57:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        int n=arr.size();
+      unordered_map<int, int> mp; 
+    int diff;
+    for(int i=0;i<n;i++){
+        diff = target - arr[i];
+
+        if(mp.find(diff) != mp.end()){
+            return true;
+        }
+
+        mp[arr[i]] = i; 
+    }
+    return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore(); // To discard any leftover newline characters
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.twoSum(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
