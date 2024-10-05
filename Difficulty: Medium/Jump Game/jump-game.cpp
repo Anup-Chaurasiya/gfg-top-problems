26899729cc8d5dcc3a7f395a390c6c36f6654ab7
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-10-05 19:01:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Jump Game/jump-game.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int nums[], int N) {
+    int match=0;
+    for(int i=0;i<N;i++){
+        
+        if(i>match) return false;
+
+        match = max(match , i+nums[i]);
+
+    }
+    return true ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
